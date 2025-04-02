c48f068ef693572c567e6170b5829bcf5b38f390
Edward Johnson <edwardsj13579@gmail.com>
2025-04-02 12:08:02 -0300

    added comments

--- ptbench/enem.py
+++ ptbench/enem.py
@@ -10,29 +10,34 @@ from lighteval.utils.language import Language
 
 def enem_pfn(line, task_name: str = None):
     """Prompt function para o dataset do ENEM."""
+
+    # Definindo algumas variaveis que são padrão para o Lighteval
     translation_literals = TRANSLATION_LITERALS[Language.PORTUGUESE]
     answer_word = capitalize(translation_literals.answer)
+
+    # Definindo a instrução para o Lighteval
     instruction = "As perguntas a seguir são questões de múltipla escolha do Exame Nacional do Ensino Médio (ENEM), selecione a única alternativa correta e responda apenas com as letras \"A\", \"B\", \"C\", \"D\" ou \"E\".\n\n"
-    
+
     description = ""
     if "description" in line and line["description"]:
-        # Removing empty list brackets
+        # Removendo listas vazias
         if isinstance(line["description"], list):
-            if line["description"]:  # Only process non-empty lists
+            if line["description"]:  # Apenas processa listas não vazias
                 description = "\n".join(str(item) for item in line["description"])
         else:
             description = str(line["description"])
-    
+
     # Format question
     question = line["question"]
     question = question.replace("[[placeholder]]", "").strip()
 
+    # Cria query dependendo se a descrição existe ou não
     query = instruction
     if description:
         query += f"{description}\n\nPergunta: {question}\n"
     else:
         query += f"Pergunta: {question}\n"
-    
+
     choices = [
         line["alternative_a"],
         line["alternative_b"],
@@ -40,17 +45,17 @@ def enem_pfn(line, task_name: str = None):
         line["alternative_d"],
         line["alternative_e"],
     ]
-    
+
     valid_keys = ["A", "B", "C", "D", "E"]
-    
+
     answer_index = line["label"]
-    answer_index = [char_to_num(answer_index)] # Make to a list
-    
-    # Build the query with question and options
+    answer_index = [char_to_num(answer_index)] # Indice da resposta correta em formato de lista
+
+    # Criar fim da query com as opções de resposta
     options_text = "\n".join([f"{key}. {choice}" for key, choice in zip(valid_keys, choices)])
     query = f"{query}{options_text}\n"
 
-    
+
     return Doc(
         task_name=task_name,
         query=query,
